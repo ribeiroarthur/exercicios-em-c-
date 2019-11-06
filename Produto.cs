4e767eaf890136a0976ecc482c209f570bb0ba38
ribeiroarthur <56894765+ribeiroarthur@users.noreply.github.com>
2019-11-06 09:34:53 -0300

    ex 05

--- Produto.cs
+++ Produto.cs
@@ -1,68 +1,64 @@
-
-
 class Produto{
-
-  Estoque  estoque = new Estoque();
- 
-  private  string data;
-  private string marca;
-  private  string tipo;
+ // aqui são os atributos 
+  private string nome;
+  private  float preco;
+  private  int quantidade;
 
   public Produto(){
-    data = "9999";
-    marca = "999";
-    tipo = "999";
+    nome = "000";
+    preco = 00;
+    quantidade = 000;
+  }
+
+  public Produto(string n, float p, int qtd){
+    nome = n;
+    preco  = p; 
+    quantidade = qtd;
+  }
+
+  public string getNome(){
+    return nome; 
+  }
+
+  public float getPreco(){
+    return preco; 
+  }
+
+  public int getQuantidade(){
+    return quantidade; 
+  }
+
+   public void setNome(string n){
+    nome = n;
+  }
+
+  public void setPreco(float p){
+    preco = p;    
+  }
+
+  public void setTamanho( int q){
+    quantidade = q;
   }
   
-  public string getData(){
-   return data;
+
+
+  public double ValorTotalEstoque(){
+    return preco*quantidade;
+
   }
   
-  public string getMarca(){
-  return marca;
+  public void Adiconar_P(int quantidade_A){
+    quantidade+=quantidade_A;
   }
-
-  public string getTipo(){
-   return tipo;
-  }
-
-  public void setData( string d ){
-    data = d;
-  }
-
-  public void setMarca( string m){
-    marca = m;
-  }
-
-  public void setTipo( string t){
-    tipo = t;
-  }
-
-  public  Produto (string d, string m , string t){
-    data = d;
-    marca =m;
-    tipo = t;
-
-  } 
-
 
   
-  public Produto  CriarProduto(string data, string marca,string tipo){
-    Produto  produto = new Produto(data, marca , tipo);
-    // estou retorna  ndo o produto  
-    estoque.IncluirProduto(produto);
-    return produto;
-       
+  public void Remover_P(int quantidade_R){
+    quantidade-=quantidade_R;
   }
-
-   public void AssociarEstoque(Estoque est){
-    estoque=est;
-  }
-
-  public string MostrarNaLista(){
-    return string.Format("Data:{0} \n Marca:{1} \n Tipo: {2}\n================",data,marca,tipo);
-  } 
-
-
   
+  public override string ToString(){
+    return nome + ", $R" + preco + ", quantidade: "+quantidade +","+
+    " valor de estoque: $R "+ValorTotalEstoque();
+
+  }
 }
--- main.cs
+++ main.cs
@@ -2,51 +2,39 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
+   
 
+   
+    Console.Write("Nome do produto:");
+    string nome = Console.ReadLine();
 
-    Console.WriteLine ("Entre com as informações do triâgulo x :");
+    Console.Write("Preço do produto:");
+    float preco = float.Parse(Console.ReadLine());
+
+    Console.Write("Quantidade  do produto:");
+    int quantidade = int.Parse(Console.ReadLine());
+
+    Produto produto =  new Produto(nome, preco , quantidade);
+
+    Console.WriteLine("Nome do produto: "+produto);
     
-    double triangulo1a=double.Parse(Console.ReadLine());
-    double triangulo1b=double.Parse(Console.ReadLine());
-    double triangulo1c=double.Parse(Console.ReadLine());
-
-    Triangulo triangulo1 = new Triangulo (triangulo1a,triangulo1b,triangulo1c);
-
-    Console.WriteLine ("Entre com as informações do triâgulo y:");
-
-    double triangulo2a=double.Parse(Console.ReadLine());
-    double triangulo2b=double.Parse(Console.ReadLine());
-    double triangulo2c=double.Parse(Console.ReadLine());
-
-    Triangulo triangulo2 = new Triangulo (triangulo2a,triangulo2b,triangulo2c);
-
-    double areaX = triangulo1.Area();
-
-    double areaY = triangulo2.Area();
-
-
-    Console.WriteLine ("Área do Triângulo X é de {0}",areaX);
-
-    Console.WriteLine ("Área do Triângulo yé de {0}",areaY);
+    Console.Write("Adiconar produto:");
+    int qte_A=int.Parse(Console.ReadLine());
 
   
-    Console.WriteLine(triangulo1.AreaMaior(areaX,areaY));
+    produto.Adiconar_P(qte_A);
+   
+
+    Console.WriteLine("Dados atualizados: "+produto);
 
 
+    Console.WriteLine("Remover produto:");
+    int qte_R=int.Parse(Console.ReadLine());
 
+    produto.Remover_P(qte_R);
+    
+    Console.Write("Dados atualizados: "+produto);
 
-    /*
-
-    if (areaX>areaY){
-      Console.WriteLine ("Maior foi a área X");
-      
-    }else if (areaX==areaY){
-      Console.WriteLine ("Mesmo tamanho");
-    }else if (areaY>areaX){
-      Console.WriteLine ("Maior foi a área Y");
-    }
-
-    */
 
   }
 }
